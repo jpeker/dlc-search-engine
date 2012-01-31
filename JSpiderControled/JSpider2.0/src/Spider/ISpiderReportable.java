e59b0fad8efd006289b52889bef593e63071c1d8
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2012-01-31 17:57:54 +0000

    Elimando el metodo mail

--- JSpiderControled/JSpider2.0/src/Spider/ISpiderReportable.java
+++ JSpiderControled/JSpider2.0/src/Spider/ISpiderReportable.java
@@ -13,6 +13,6 @@ import java.net.URL;
 public interface ISpiderReportable {
     public boolean spiderFoundURL(URL base, URL url);
     public void spiderFoundURLError(URL url);
-    public void spiderFoundEMail(String email);
+    //public void spiderFoundEMail(String email);
     
 }
