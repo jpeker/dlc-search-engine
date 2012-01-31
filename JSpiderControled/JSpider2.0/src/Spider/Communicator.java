e34de3f13cd7459992a9cdb56ac4523ff0bf768d
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2012-01-31 17:58:23 +0000

    Clase Communicator.java clase donde debemos trabajar.

---
+++ JSpiderControled/JSpider2.0/src/Spider/Communicator.java
@@ -0,0 +1,97 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Spider;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ *
+ * @author Administrador
+ */
+public class Communicator implements Runnable,ISpiderReportable{
+   /* The background spider thread */
+  protected Thread backgroundThread;
+
+  /**
+   * The spider object being used
+   */
+  protected String textToCrawl;
+  protected Spider spider;
+
+  /**
+   * The URL that the spider began with
+   */
+  protected URL base;
+
+   /**
+   * How many bad links have been found
+   */
+  protected int badLinksCount = 0;
+
+  /**
+   * How many good links have been found
+   */
+  protected int goodLinksCount = 0;
+
+  public void beginCrawler(String textToCrawl){
+     if ( backgroundThread==null ) {
+        backgroundThread = new Thread(this);
+        backgroundThread.start();
+        this.textToCrawl=textToCrawl;
+        goodLinksCount=0;
+        badLinksCount=0;
+    }
+    else {
+        spider.cancel();
+    }
+}
+    public void run() {
+      try {
+        spider = new Spider(this);
+        spider.clear();
+        base = new URL(textToCrawl);
+        spider.addURL(base);
+        spider.begin();
+        backgroundThread=null;
+    }
+      catch ( MalformedURLException e ) {
+        System.out.println("error "+ e.toString());
+    }
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+ public boolean spiderFoundURL(URL base, URL url) {
+    if ( !checkLink(url) ) {
+        badLinksCount++;
+        return false;
+     }
+    goodLinksCount++;
+    if ( !url.getHost().equalsIgnoreCase(base.getHost()) )
+      return false;
+    else
+      return true;
+  }
+  
+  protected boolean checkLink(URL url)
+  {
+    try {
+      URLConnection connection = url.openConnection();
+      connection.connect();   
+      return true;
+    } catch ( IOException e ) {
+      return false;
+    }
+        //throw new UnsupportedOperationException("Not supported yet.");
+    }
+    public void spiderFoundURLError(URL url) {
+        badLinksCount++;
+        System.out.println(url +" gots errors");
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
