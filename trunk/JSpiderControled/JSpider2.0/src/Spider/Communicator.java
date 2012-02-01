66397e4df87d7b34463445291c4bed607a53f65f
julianpeker@gmail.com <julianpeker@gmail.com@eec16b6f-d674-0caf-973c-04c45c5acc91>
2012-02-01 20:17:44 +0000

    comentario de Communicator.java

--- trunk/JSpiderControled/JSpider2.0/src/Spider/Communicator.java
+++ trunk/JSpiderControled/JSpider2.0/src/Spider/Communicator.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package Spider;
 
 import java.io.IOException;
@@ -11,10 +6,6 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
 
-/**
- *
- * @author Administrador
- */
 public class Communicator implements Runnable,ISpiderReportable{
    /* The background spider thread */
   protected Thread backgroundThread;
@@ -22,8 +13,8 @@ public class Communicator implements Runnable,ISpiderReportable{
   /**
    * The spider object being used
    */
-  protected String textToCrawl;
-  protected Spider spider;
+  protected String textToCrawl;// variable usadas para la urls que usa el crawler
+  protected Spider spider;//objeto que usamos para crawlear las urls
 
   /**
    * The URL that the spider began with
@@ -39,13 +30,16 @@ public class Communicator implements Runnable,ISpiderReportable{
    * How many good links have been found
    */
   protected int goodLinksCount = 0;
-  private HashMap pages;
+  private HashMap pages;//HashMap con todas las urls y su contenido util
+  /* metodo que recibe un link como string, se utiliza para dar comienzo el proceso del crawler
+   * y devuelve el HashMap con las urls y su contenidos
+   */
   public HashMap beginCrawler(String textToCrawl){
      if ( backgroundThread==null ) {
       this.textToCrawl=textToCrawl;
       //  backgroundThread = new Thread(this);
        // backgroundThread.start();
-     this.runy();
+     this.initSpider();
      goodLinksCount=0;
         badLinksCount=0;
        return getHashMapPages();  
@@ -62,6 +56,8 @@ public class Communicator implements Runnable,ISpiderReportable{
   {
     return spider.getHashMapPages();
   }
+
+
     public void run()  {
       try {
         spider = new Spider(this);
@@ -79,35 +75,44 @@ public class Communicator implements Runnable,ISpiderReportable{
 
 
     }
-  public void runy()  {
+
+    /*Metodo que da inicio el spider, crea la url , verifica que este bien formado si no tira una excepcion
+     *
+     */
+  public void initSpider()  {
       try {
-        spider = new Spider(this);
+        spider = new Spider(this);// Creo el objeto spider
         spider.clear();
-        base = new URL(textToCrawl);
-        spider.addURL(base);
-          spider.begin();
-          
-        backgroundThread=null;
-    }
+        base = new URL(textToCrawl);//creo la url con el link en forma de string
+        spider.addURL(base); //agrego la url como base
+        spider.begin();//comienza a crawlear
+         }
       catch ( MalformedURLException e ) {
         System.out.println("error "+ e.toString());
-        
-    }
-
-
-    }
+        }
+}
+  /*
+   * es llamado por el spyder cuando encuentra la url, verifica si es buena y luego valida si pertene al mismo dominio
+   * que la base
+   * recibe la base y una url
+   * devuelve true si pertence al mismo domnio
+   */
  public boolean spiderFoundURL(URL base, URL url) {
-    if ( !checkLink(url) ) {
+    if ( !checkLink(url) ) { //verifica que sea buena la url
         badLinksCount++;
         return false;
      }
     goodLinksCount++;
-    if ( !url.getHost().equalsIgnoreCase(base.getHost()) )
+    if ( !url.getHost().equalsIgnoreCase(base.getHost()) ) //valida que sea del mismo dominio que la base
       return false;
     else
       return true;
   }
-  
+  /*
+   * Checkea que link sea bueno
+   * recibe una url
+   * devuelve true si es bueno el link
+   */
   protected boolean checkLink(URL url)
   {
     try {
@@ -117,8 +122,11 @@ public class Communicator implements Runnable,ISpiderReportable{
     } catch ( IOException e ) {
       return false;
     }
-        //throw new UnsupportedOperationException("Not supported yet.");
-    }
+  }
+  /*
+   * Es llamado cuando hay un error en el link
+   * recibe una url
+   */
     public void spiderFoundURLError(URL url) {
         badLinksCount++;
         System.out.println(url +" gots errors");
