a1b0a4f83d76335ad80ee4d4f9e9394c75caed9f
julianpeker@gmail.com <julianpeker@gmail.com@eec16b6f-d674-0caf-973c-04c45c5acc91>
2012-02-07 19:55:03 +0000

    funcion de postlist

--- trunk/Scripts/02.funciones.sql
+++ trunk/Scripts/02.funciones.sql
@@ -94,7 +94,7 @@ CREATE OR REPLACE FUNCTION fn_Save_Postlist (
   
    
   
-) RETURNS SMALLINT AS $$
+) RETURNS VOID AS $$
 
     DECLARE
         var_id_Word                INTEGER         :=0;
@@ -117,46 +117,52 @@ CREATE OR REPLACE FUNCTION fn_Save_Postlist (
             WHERE u.url_Name =  var_url_Name;
                 -- veo si existe las word y la page
             IF (var_id_Word  > 0 and var_id_Url  > 0) THEN
-              SELECT COUNT(*)
+             
+			 SELECT COUNT(*)
               INTO var_count
               FROM  PostList p
-              WHERE p.id_Word  =  var_id_Word and p. id_Url = var_id_Url 
-               -- veo si existe
+              WHERE p.id_Word  =  var_id_Word and p. id_Url = var_id_Url; 
+               -- veo si existe el postlist
               IF (var_count > 0) THEN
-              UPDATE Word w SET -- sí existe ==> update
-                name_Word  =   var_name_Word,
-                nr   =  var_nr,
-                max_Tf =  var_max_Tf 
-                WHERE w.id_Word =     var_id_Word ;
-        ELSE -- no existe ==> insert
-            IF (var_idusuario IS NULL) THEN
-                  var_id_Word  :=fn_Get_Id_Word ();
+              UPDATE PostList p SET -- sí existe ==> update
+                frequency = var_frequency   		
+               WHERE p.id_Word  =  var_id_Word and p. id_Url = var_id_Url;
+              ELSE -- no existe ==> insert
+                  INSERT INTO PostList(id_Word, id_Url, frequency)
+                  VALUES ( var_id_Word, var_id_Url,var_frequency);
+              END IF;
             END IF;
-            INSERT INTO Word(id_Word, name_Word, nr,max_Tf)
-                VALUES ( var_id_Word, var_name_Word,var_nr,var_max_Tf);
-        END IF;
-        END IF;
 
-        RETURN var_idusuario;
+        RETURN ;
     END;
 $$ LANGUAGE plpgsql;
--- La siguiente función elimina una Word de la BBDD.
-CREATE OR REPLACE FUNCTION pr_deleteWord (
-     pin_id_Word                     INTEGER
+-- La siguiente función elimina una postlist de la BBDD.
+CREATE OR REPLACE FUNCTION pr_deletePostList(
+     pin_name_Word                  VARCHAR,
+    pin_url_Name                 VARCHAR
 ) RETURNS VOID AS $$
 
-    DECLARE   
-    var_count                    INTEGER         := 0;
+    DECLARE
+           var_id_Word             INTEGER         :=0;
+           var_id_Url              INTEGER         :=0;	
+           var_url_Name			   VARCHAR		   := TRIM(  pin_url_Name);
+		   var_name_Word		   VARCHAR		   := TRIM(pin_name_Word);
+		   var_count               INTEGER         := 0;	
     BEGIN
-             -- Cuento amigos del usuario a eliminar
-            SELECT COUNT(*)
-            INTO var_count
-            FROM  PostList p
-            WHERE p.id_Word  =  pin_id_Word ;
-
-            IF (var_count = 0) THEN -- si no tiene amigo lo elimino
-            DELETE FROM  Word w
-            WHERE w.id_Word = pin_id_Word;
+	       -- recupero el id de la word
+            SELECT w.id_Word
+            INTO  var_id_Word 
+            FROM  Word w
+            WHERE w.name_Word  = var_name_Word;
+            -- recupero el id de la page
+			SELECT w.id_Url
+            INTO  var_id_Url
+            FROM  Page u
+            WHERE u.url_Name =  var_url_Name;
+                -- veo si existe las word y la page y elimino
+            IF (var_id_Word  > 0 and var_id_Url  > 0) THEN
+              DELETE FROM  PostList p
+              WHERE p.id_Word  =  var_id_Word and p. id_Url = var_id_Url; 
             END IF;
 
         RETURN;
