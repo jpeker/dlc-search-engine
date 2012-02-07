284874f648fb0cc875e46855257f0988c8629261
julianpeker <julianpeker@gmail.com>
2012-02-07 19:41:24 +0000

    correcion de word

--- Scripts/02.funciones.sql
+++ Scripts/02.funciones.sql
@@ -29,11 +29,11 @@ CREATE OR REPLACE FUNCTION fn_Save_Word (
     DECLARE
         var_id_Word                INTEGER         :=  pin_id_Word;
         var_name_Word              VARCHAR         := TRIM( pin_name_Word );
-        var_nr                     INTEGER         :=  pin_nr   
-        var_max_Tf                 INTEGER         :=  pin_max_Tf 
+        var_nr                     INTEGER         :=  pin_nr ;  
+        var_max_Tf                 INTEGER         :=  pin_max_Tf; 
         var_count                  INTEGER         := 0;
  BEGIN
-            -- cuento usuarios
+            -- cuento words
             SELECT COUNT(*)
             INTO var_count
             FROM Word w
@@ -48,14 +48,12 @@ CREATE OR REPLACE FUNCTION fn_Save_Word (
              
             WHERE w.id_Word =     var_id_Word ;
         ELSE -- no existe ==> insert
-            IF (var_idusuario IS NULL) THEN
-                  var_id_Word  :=fn_Get_Id_Word ();
-            END IF;
+           
             INSERT INTO Word(id_Word, name_Word, nr,max_Tf)
                 VALUES ( var_id_Word, var_name_Word,var_nr,var_max_Tf);
         END IF;
 
-        RETURN var_idusuario;
+        RETURN var_id_Word;
     END;
 $$ LANGUAGE plpgsql;
 -- La siguiente función elimina una Word de la BBDD.
@@ -66,7 +64,7 @@ CREATE OR REPLACE FUNCTION pr_deleteWord (
     DECLARE   
     var_count                    INTEGER         := 0;
     BEGIN
-             -- Cuento amigos del usuario a eliminar
+             -- Cuento postlist del usuario a eliminar
             SELECT COUNT(*)
             INTO var_count
             FROM  PostList p
@@ -81,7 +79,7 @@ CREATE OR REPLACE FUNCTION pr_deleteWord (
     END;
 $$ LANGUAGE plpgsql;
 -- =============================================================================
--- PostLIST
+-- PostLisT
 -- =============================================================================
 
 -- La siguiente función "guarda" una postlist en la BBDD.
@@ -89,9 +87,9 @@ $$ LANGUAGE plpgsql;
 -- Entre ellos, el id.
 -- Si existe lo actualiza.
 -- Si no existe lo crea.
-CREATE OR REPLACE FUNCTION fn_Save_Postlit (
-   pin_id_Word                   INTEGER,
-    pin_id_Url                   INTEGER,
+CREATE OR REPLACE FUNCTION fn_Save_Postlist (
+    pin_name_Word                  VARCHAR,
+    pin_url_Name                 VARCHAR,
     pin_frequency                INTEGER
   
    
@@ -99,32 +97,44 @@ CREATE OR REPLACE FUNCTION fn_Save_Postlit (
 ) RETURNS SMALLINT AS $$
 
     DECLARE
-        var_id_Word                INTEGER         :=  pin_id_Word;
-        var_id_Url                 INTEGER         := pin_id_Url;
-        var_frequency              INTEGER         := pin_frequency ; 
+        var_id_Word                INTEGER         :=0;
+        var_id_Url                 INTEGER         :=0;
+        var_url_Name			   VARCHAR		   := TRIM(  pin_url_Name);
+		var_name_Word			   VARCHAR		   := TRIM(pin_name_Word);
+		var_frequency              INTEGER         := pin_frequency ; 
         var_count                  INTEGER         := 0;		
      
  BEGIN
-            -- cuento usuarios
-            SELECT COUNT(*)
-            INTO var_count
-            FROM PostList p
-            WHERE p.id_Word =  var_id_Word AND p;
-            
-                -- veo si existe
-            IF (var_count > 0) THEN
-            UPDATE Word w SET -- sí existe ==> update
+            -- recupero el id de la word
+            SELECT w.id_Word
+            INTO  var_id_Word 
+            FROM  Word w
+            WHERE w.name_Word  = var_name_Word;
+            -- recupero el id de la page
+			SELECT w.id_Url
+            INTO  var_id_Url
+            FROM  Page u
+            WHERE u.url_Name =  var_url_Name;
+                -- veo si existe las word y la page
+            IF (var_id_Word  > 0 and var_id_Url  > 0) THEN
+              SELECT COUNT(*)
+              INTO var_count
+              FROM  PostList p
+              WHERE p.id_Word  =  var_id_Word and p. id_Url = var_id_Url 
+               -- veo si existe
+              IF (var_count > 0) THEN
+              UPDATE Word w SET -- sí existe ==> update
                 name_Word  =   var_name_Word,
-               nr   =  var_nr,
+                nr   =  var_nr,
                 max_Tf =  var_max_Tf 
-             
-            WHERE w.id_Word =     var_id_Word ;
+                WHERE w.id_Word =     var_id_Word ;
         ELSE -- no existe ==> insert
             IF (var_idusuario IS NULL) THEN
                   var_id_Word  :=fn_Get_Id_Word ();
             END IF;
             INSERT INTO Word(id_Word, name_Word, nr,max_Tf)
                 VALUES ( var_id_Word, var_name_Word,var_nr,var_max_Tf);
+        END IF;
         END IF;
 
         RETURN var_idusuario;
