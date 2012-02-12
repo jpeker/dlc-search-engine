c17565bc19fdbbd7b90799159676e222e6093659
julianpeker@gmail.com <julianpeker@gmail.com@eec16b6f-d674-0caf-973c-04c45c5acc91>
2012-02-12 16:36:34 +0000

    metodo de documentos candidatos

--- trunk/JDBAccessLibrary/src/postgresql/PostgreSQLPostListDAO.java
+++ trunk/JDBAccessLibrary/src/postgresql/PostgreSQLPostListDAO.java
@@ -11,6 +11,7 @@ import beans.Word;
 import java.util.LinkedList;
 import java.sql.*;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections.*;
 import java.util.Enumeration;
@@ -22,5 +23,66 @@ import java.util.Iterator;
  * @author Julian
  */
 public class PostgreSQLPostListDAO {
+    
+      public static void main(String[] args) {
+        //Word w0= new Word("Fede", 8, 2);
+        
+       PostgreSQLPostListDAO paldao= new  PostgreSQLPostListDAO();
+        //paldao.grabarPalabra(w0);
+        Word w1= new Word("casa", 8, 2);
+       Word w2= new Word("cara", 8, 2);
+        Word w3= new Word("Julián", 8, 2); 
+        ArrayList<Word> worda= new ArrayList();
+        worda.add(w1);
+        worda.add(w2);
+        worda.add(w3);
+        Iterator i=paldao.obtenerDocumentoCandidatos(worda).iterator();
+        while(i.hasNext())
+        {
+            Document doc =(Document)i.next();
+            System.out.println(doc.toString());
+        }
+        // TODO code application logic here
+    }
+    public ArrayList<Document> obtenerDocumentoCandidatos( ArrayList<Word> words )
+    {
+     ArrayList<Document> ret=new ArrayList();
+     ArrayList<String> cadena= new ArrayList();
+     Iterator i = words.iterator();
+     StringBuilder cad = new StringBuilder();
+     while(i.hasNext())
+     {
+     Word word =(Word)i.next();
+     cad.append("'");
+    cad.append(word.getName());
+    cad.append("'");
+    if(i.hasNext())cad.append(",");
+     }
+    PreparedStatement st;
+        Connection con;
+      try {
+            con = PostgreDBManager.getConnection();
+            synchronized(con)
+            {
+                String query="select distinct d.url_Name from postlist p inner join page d on p.id_Url = d.id_Url inner join word w on p.id_Word = w.id_Word  where w.name_Word in ("+cad.toString()+")";
+                st = con.prepareStatement(query);
+                ResultSet results=st.executeQuery();
+                while(results.next())
+                {
+                   ret.add(new Document("doc"+results.getRow(),results.getString(1),""));
+                }
+                results.close();
+                st.close();
+            }
+        } catch (SQLException ex) {
+            ret=null;
+        }
+       
+    
+    
+    
+    return ret;
+    
+    }
     
 }
