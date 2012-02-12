60f301fe001d6366affcb665bfd6b089d6e52821
rodrigo.inf.liberal <rodrigo.inf.liberal@gmail.com>
2012-02-12 21:41:48 +0000

    JavaNotificatorSearchEngineLibrary subida

---
+++ JavaNotificatorSearchEngineLibrary/src/Notificator/NotificatorInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Notificator;
+import java.util.ArrayList;
+
+/**
+ * Esta interface comunica operaciones efectuadas entre los modulos del
+ * todo el proyecto search engine.
+ * 
+ * @author Altamirado Liberal Peker
+ */
+public interface NotificatorInterface {
+    public void sendNotification(String notificaton);
+    public String recieveNotification();
+    public void sendSetOfNotifications(ArrayList<String> notificaton);
+    public ArrayList<String> recieveNotificationSetOfNotifications();
+}
