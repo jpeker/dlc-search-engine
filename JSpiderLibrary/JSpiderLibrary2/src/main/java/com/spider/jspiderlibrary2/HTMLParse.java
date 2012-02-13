847dab0cdf2719fefbca7e340b33fdfd558550b6
julianpeker <julianpeker@gmail.com>
2012-02-13 14:25:07 +0000

    New JSpiderLibrary with maven support

---
+++ JSpiderLibrary/JSpiderLibrary2/src/main/java/com/spider/jspiderlibrary2/HTMLParse.java
@@ -0,0 +1,13 @@
+/*
+ * HTMLParse clase que extiende de HTMLEditorKit para parsear streams de HTML
+ */
+package com.spider.jspiderlibrary2;
+//HTML Editor clase para reconocer e implementar documentos de html, basado en HTML 3.2 sin implementacion con HTML 4 aun
+import javax.swing.text.html.HTMLEditorKit;
+public class HTMLParse extends HTMLEditorKit {
+//Trae el parser de HTML que se utilizará para la lectura de streams de HTML.
+  public HTMLEditorKit.Parser getParser()
+  {
+    return super.getParser();
+  }
+}
