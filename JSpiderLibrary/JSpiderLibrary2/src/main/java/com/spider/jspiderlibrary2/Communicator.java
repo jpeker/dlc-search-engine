9d1bd63cf2fb9fb66666352dd57450c49758787b
julianpeker <julianpeker@gmail.com>
2012-02-13 14:44:49 +0000

    todo listo

--- JSpiderLibrary/JSpiderLibrary2/src/main/java/com/spider/jspiderlibrary2/Communicator.java
+++ JSpiderLibrary/JSpiderLibrary2/src/main/java/com/spider/jspiderlibrary2/Communicator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Communicator implements Runnable,ISpiderReportable{
