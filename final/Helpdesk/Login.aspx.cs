ffc30cd03c5edcbe7f6db461d9df1a4be2ca13ba
julianpeker <julianpeker@gmail.com>
2012-11-09 15:32:35 +0000

    guarfd

--- final/Helpdesk/Login.aspx.cs
+++ final/Helpdesk/Login.aspx.cs
@@ -12,4 +12,13 @@ public partial class Login : System.Web.UI.Page
         helpdesk hdMaster = (helpdesk)this.Master;
         ((Label)hdMaster.FindControl("lblTitulo")).Text = "Login";
     }
+    protected void txtLogin_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        if (Seguridad.ValidarUsuario(txtLogin.UserName, txtLogin.Password))
+        {
+          
+            Console.WriteLine("si existe");
+           String roles= Seguridad.ObtenerRoles(txtLogin.UserName);
+        }
+    }
 }
