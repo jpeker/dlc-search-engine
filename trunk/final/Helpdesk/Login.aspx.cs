60f4066abf8ab09a4dcec79a05b8001f64cb5a42
julianpeker@gmail.com <julianpeker@gmail.com@eec16b6f-d674-0caf-973c-04c45c5acc91>
2012-11-10 14:52:07 +0000

    m

--- trunk/final/Helpdesk/Login.aspx.cs
+++ trunk/final/Helpdesk/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -16,9 +17,21 @@ public partial class Login : System.Web.UI.Page
     {
         if (Seguridad.ValidarUsuario(txtLogin.UserName, txtLogin.Password))
         {
-          
+            e.Authenticated = true;
+
             Console.WriteLine("si existe");
-           String roles= Seguridad.ObtenerRoles(txtLogin.UserName);
+            String roles = Seguridad.ObtenerRoles(txtLogin.UserName);
+            FormsAuthenticationTicket autTicket = new FormsAuthenticationTicket(1, txtLogin.UserName, DateTime.Now, DateTime.Now.AddMinutes(60), false, roles);
+            //Encriptar el ticket
+            string encrTicket = FormsAuthentication.Encrypt(autTicket);
+            // Crea una cookie con el ticket encriptado
+            HttpCookie autCookie = new HttpCookie("testseguridad", encrTicket);
+            // Agrega la cookie a la Response
+            Response.Cookies.Add(autCookie);
+            // Redirecciona al usuario a la página que solicitó
+
+            Response.Redirect(FormsAuthentication.GetRedirectUrl(txtLogin.UserName, false));
         }
+        else { }
     }
 }
